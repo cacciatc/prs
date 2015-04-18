0e84e963692b7c42a7e201b55029b04dd4bce347
Will Oller <will@willoller.com>
2015-04-18 15:10:50 -0700

    New AIs

--- src/ai.js
+++ src/ai.js
@@ -21,3 +21,13 @@ var rockman = function(){};
 rockman.prototype  = AI;
 rockman.tell  = function(gs) { return ROCK; }
 rockman.shoot = function(gs) { gs.engine.shoot_ai(ROCK); }
+
+var scissorman = function(){};
+scissorman.prototype  = AI;
+scissorman.tell  = function(gs) { return SCISSORS; }
+scissorman.shoot = function(gs) { gs.engine.shoot_ai(SCISSORS); }
+
+var paperman = function(){};
+paperman.prototype  = AI;
+paperman.tell  = function(gs) { return PAPER; }
+paperman.shoot = function(gs) { gs.engine.shoot_ai(PAPER); }
