35ccc7309f4a050142f8379ace98091a3011206d
Chris <chris.cacciatore@gmail.com>
2015-04-18 10:34:52 -0700

    init

---
+++ src/game.js
@@ -0,0 +1,13 @@
+Maze.Game = function(game) {};
+Maze.Game.prototype = {
+    create: function() {},
+    initLevels: function() {},
+    showLevel: function(level) {},
+    updateCounter: function() {},
+    managePause: function() {},
+    manageAudio: function() {},
+    update: function() {},
+    wallCollision: function() {},
+    handleOrientation: function(e) {},
+    finishLevel: function() {}
+};
