0fb4d53415af32b29ef23abb5d01b45ab4610ae2
Will Oller <will@willoller.com>
2015-04-19 13:27:23 -0700

    AI now weights character preferences 3/5 times at random

--- src/ai.js
+++ src/ai.js
@@ -1,5 +1,6 @@
 var character = function() {
     var health = 100;
+    var next_shoot = null;
 
     return {
         tieShoot:  function(){},
@@ -9,27 +10,45 @@ var character = function() {
         },
         getHealth: function(){
             return health;
-        }
+        },
+        tell: function(gs) { 
+          if (!next_shoot){
+            next_shoot = this.nextShoot(gs);
+          }
+          return next_shoot;
+        },
+        shoot: function(gs) {
+          if (!next_shoot){
+            next_shoot = this.nextShoot(gs);
+          }
+
+          var result = next_shoot;
+          next_shoot = null; // this getter resets its value
+          return result;
+        },
     }
 }
 
 var rockman = function(){
-   var c = character();
-   c.tell  = function(gs) { return ROCK; }
-   c.shoot = function(gs) { return ROCK; }
-   return c;
+  var c = character();
+  c.nextShoot = function(gs) {
+    return [ROCK, PAPER, SCISSORS, ROCK, ROCK][Math.floor(Math.random() * 5)];
+  };
+  return c;
 };
 
 var scissorman = function(){
-   var c = character();
-   c.tell  = function(gs) { return SCISSORS; }
-   c.shoot = function(gs) { return SCISSORS; }
-   return c;
+  var c = character();
+  c.nextShoot = function(gs) {
+    return [ROCK, PAPER, SCISSORS, SCISSORS, SCISSORS][Math.floor(Math.random() * 5)];
+  };
+  return c;
 };
 
 var paperman = function(){
-   var c = character();
-   c.tell  = function(gs) { return PAPER; }
-   c.shoot = function(gs) { return PAPER; }
-   return c;
+  var c = character();
+  c.nextShoot = function(gs) {
+    return [ROCK, PAPER, SCISSORS, PAPER, PAPER][Math.floor(Math.random() * 5)];
+  };
+  return c;
 };
