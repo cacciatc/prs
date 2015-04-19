d59ecc0011a627ce906a31ab2d06a0cdbb56694d
Will Oller <will@willoller.com>
2015-04-18 23:18:16 -0700

    Fixed dumb AIs to use right r/p/s

--- src/ai.js
+++ src/ai.js
@@ -22,14 +22,14 @@ var rockman = function(){
 
 var scissorman = function(){
    var c = character();
-   c.tell  = function(gs) { return ROCK; }
-   c.shoot = function(gs) { return ROCK; }
+   c.tell  = function(gs) { return SCISSORS; }
+   c.shoot = function(gs) { return SCISSORS; }
    return c;
 };
 
 var paperman = function(){
    var c = character();
-   c.tell  = function(gs) { return ROCK; }
-   c.shoot = function(gs) { return ROCK; }
+   c.tell  = function(gs) { return PAPER; }
+   c.shoot = function(gs) { return PAPER; }
    return c;
 };
